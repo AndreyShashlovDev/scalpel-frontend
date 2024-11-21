cc6ddc473cdf3a0febd78f04587025f453366fb7
AndreyShashlov <cgmgamesdev@gmail.com>
2024-11-21 16:13:35 +0300

    fix analyics chart view.

--- src/feature/strategies/analytics/presentation/mapping/AnalyticsResponseToSwapPriceUiModel.ts
+++ src/feature/strategies/analytics/presentation/mapping/AnalyticsResponseToSwapPriceUiModel.ts
@@ -3,21 +3,25 @@ import { AnalyticsChartUiModel } from '../model/AnalyticsChartUiModel.ts'
 
 export const AnalyticsResponseToSwapPriceUiModel = (response: AnalyticsResponse): AnalyticsChartUiModel => {
 
-  const roundToMinute = (unixtime: number) => Math.floor(unixtime / 60) * 60
+  const roundToTenMinutes = (unixtime: number) => Math.floor(unixtime / 600) * 600
   const toUsdt = (value: string): number => Number((Number(value) / 10 ** 6).toFixed(2))
 
-  const swapAByDate = new Map(response.swapsCurrencyA.map(item => [
-    roundToMinute(item.date),
-    toUsdt(item.value)
-  ]))
-  const swapBByDate = new Map(response.swapsCurrencyB.map(item => [
-    roundToMinute(item.date),
-    toUsdt(item.value)
-  ]))
+// maps rounded by 10 minutes
+  const swapAByDate = new Map(
+    response.swapsCurrencyA.map(item => [
+      roundToTenMinutes(item.date),
+      toUsdt(item.value),
+    ])
+  )
+  const swapBByDate = new Map(
+    response.swapsCurrencyB.map(item => [
+      roundToTenMinutes(item.date),
+      toUsdt(item.value),
+    ])
+  )
 
-  // Основной результат
   const result = response.priceCurrencyB.map(priceBItem => {
-    const roundedDate = roundToMinute(priceBItem.date)
+    const roundedDate = roundToTenMinutes(priceBItem.date)
 
     return {
       date: new Date(roundedDate * 1000).toISOString(),
