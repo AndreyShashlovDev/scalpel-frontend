41df2999b7317be891a29431848450f2dc0177f5
AndreyShashlov <cgmgamesdev@gmail.com>
2025-03-23 11:23:47 +0300

    index chunk size reduce.

--- src/common/di/EthereumServiceStrategyModule.ts
+++ src/common/di/EthereumServiceStrategyModule.ts
@@ -1,19 +1,20 @@
 import { Module } from '../../utils/di-core/di/Dependency.ts'
 import { ChainType } from '../repository/data/model/ChainType.ts'
-import { EthereumServiceImpl } from '../service/ethereum-service/EthereumServiceImpl.ts'
 import { EthereumServiceStrategy } from '../service/ethereum-service/EthereumServiceStrategy.ts'
-import { EthereumServiceStrategyImpl } from '../service/ethereum-service/EthereumServiceStrategyImpl.ts'
 
 @Module({
   providers: [
     {
       provide: EthereumServiceStrategy,
-      useFactory: () => {
-        return new EthereumServiceStrategyImpl(
+      useFactory: async () => {
+        const module = await import('../service/ethereum-service/EthereumServiceStrategyImpl.ts')
+        const moduleEthService = await import('../service/ethereum-service/EthereumServiceImpl.ts')
+
+        return new module.EthereumServiceStrategyImpl(
           new Map([
             [
               ChainType.ETHEREUM_MAIN_NET,
-              new EthereumServiceImpl(
+              new moduleEthService.EthereumServiceImpl(
                 ChainType.ETHEREUM_MAIN_NET,
                 'https://ethereum-rpc.publicnode.com',
                 '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
@@ -21,7 +22,7 @@ import { EthereumServiceStrategyImpl } from '../service/ethereum-service/Ethereu
             ],
             [
               ChainType.POLYGON,
-              new EthereumServiceImpl(
+              new moduleEthService.EthereumServiceImpl(
                 ChainType.POLYGON,
                 'https://1rpc.io/matic',
                 '0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507',
